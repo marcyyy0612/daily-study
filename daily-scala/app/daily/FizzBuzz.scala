766827e738af2fe2b989d099fe3a8aa047ba1bba
marcyyy0612 <marcyyy0612@gmail.com>
2019-08-27 19:04:00 +0900

    add answer example

--- daily-scala/app/daily/FizzBuzz.scala
+++ daily-scala/app/daily/FizzBuzz.scala
@@ -2,6 +2,11 @@ package daily
 
 class FizzBuzz {
   def execute(num: Int) = {
-    num
+    num match {
+      case n if (n % 3 == 0 && n % 5 == 0) => "FizzBuzz"
+      case n if (n % 3 == 0) => "Fizz"
+      case n if (n % 5 == 0) => "Buzz"
+      case n => n.toString
+    }
   }
 }
--- daily-scala/test/daily/FizzBuzzSpec.scala
+++ daily-scala/test/daily/FizzBuzzSpec.scala
@@ -12,13 +12,72 @@ class FizzBuzzSpec extends FunSpec {
   describe("#execute") {
     val fizzBuzz = new FizzBuzz
 
-    describe("give 3") {
-      it("return Fizz") {
-        val expect = "Fizz"
-        val actual = fizzBuzz.execute(3)
-        assert(actual == expect)
+    describe("3の倍数のとき") {
+      describe("3のとき") {
+        it("return Fizz") {
+          val expect = "Fizz"
+          val actual = fizzBuzz.execute(3)
+          assert(actual == expect)
+        }
+      }
+      describe("6のとき") {
+        it("return Fizz") {
+          val expect = "Fizz"
+          val actual = fizzBuzz.execute(6)
+          assert(actual == expect)
+        }
+      }
+    }
+
+    describe("5の倍数のとき") {
+      describe("5のとき") {
+        it("return Buzz") {
+          val expect = "Buzz"
+          val actual = fizzBuzz.execute(5)
+          assert(actual == expect)
+        }
+      }
+      describe("10のとき") {
+        it("return Buzz") {
+          val expect = "Buzz"
+          val actual = fizzBuzz.execute(10)
+          assert(actual == expect)
+        }
+      }
+    }
+
+    describe("3と5の公倍数のとき") {
+      describe("15のとき") {
+        it("return FizzBuzz") {
+          val expect = "FizzBuzz"
+          val actual = fizzBuzz.execute(15)
+          assert(actual == expect)
+        }
+      }
+      describe("30のとき") {
+        it("return FizzBuzz") {
+          val expect = "FizzBuzz"
+          val actual = fizzBuzz.execute(30)
+          assert(actual == expect)
+        }
+      }
+    }
+
+    describe("3の倍数でも、5の倍数でもないとき") {
+      describe("4のとき") {
+        it("return 4") {
+          val expect = "4"
+          val actual = fizzBuzz.execute(4)
+          assert(actual == expect)
+        }
+      }
+      describe("8のとき") {
+        it("return 8") {
+          val expect = "8"
+          val actual = fizzBuzz.execute(8)
+          assert(actual == expect)
+        }
       }
     }
   }
 }
-
