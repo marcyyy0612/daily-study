d7cdb6370164b93363a5503302c85081bc869a03
Masahiro Kumatani <masahiro.kumatani@bizreach.co.jp>
2019-08-28 00:28:28 +0900

    rename files

--- daily-angular/src/app.ts
+++ daily-angular/src/app.ts
@@ -1,16 +1,16 @@
 import * as angular from 'angular';
 import 'angular-ui-router';
-import hello from './hello.ts';
+import fizzBuzzComponent from './fizz-buzz-component.ts';
 
 const app = angular.module('myApp', ['ui.router']);
 
-hello();
+fizzBuzzComponent();
 
 app.config(($stateProvider: any) => {
     const helloState = {
-        name: 'hello',
-        url: '/hello',
-        template: '<hello></hello>'
+        name: 'fizzBuzz',
+        url: '/fizz-buzz',
+        template: '<fizz-buzz></fizz-buzz>'
     };
     $stateProvider.state(helloState);
 });
