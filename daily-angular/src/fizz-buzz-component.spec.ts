0f98b5716c07727dd5bc2c9825586492ff350f2d
marcyyy0612 <marcyyy0612@gmail.com>
2019-08-28 12:41:16 +0900

    add answer example typescript fizzbuzz

--- daily-angular/src/fizz-buzz-component.spec.ts
+++ daily-angular/src/fizz-buzz-component.spec.ts
@@ -1,10 +1,54 @@
 import {fizzBuzz} from "./fizz-buzz-component";
 
-fdescribe('fizzBuzz', () => {
+describe('fizzBuzz', () => {
+  describe('3と5の倍数以外を渡した場合', () => {
     describe('1を渡した場合', () => {
-        it('1を返すこと', () => {
-            expect(fizzBuzz(1)).toBe(1)
-        })
-    })
-});
+      it('1を返すこと', () => {
+        expect(fizzBuzz(1)).toBe(1)
+      })
+    });
+    describe('4を渡した場合', () => {
+      it('4を返すこと', () => {
+        expect(fizzBuzz(4)).toBe(4)
+      })
+    });
+  });
+  describe('3の倍数を渡した場合', () => {
+    describe('3を渡した場合', () => {
+      it('Fizzを返すこと', () => {
+        expect(fizzBuzz(3)).toBe('Fizz')
+      })
+    });
+    describe('6を渡した場合', () => {
+      it('Fizzを返すこと', () => {
+        expect(fizzBuzz(6)).toBe('Fizz')
+      })
+    });
+  });
 
+  describe('5の倍数を渡した場合', () => {
+    describe('5を渡した場合', () => {
+      it('Buzzを返すこと', () => {
+        expect(fizzBuzz(5)).toBe('Buzz')
+      })
+    });
+    describe('10を渡した場合', () => {
+      it('Buzzを返すこと', () => {
+        expect(fizzBuzz(10)).toBe('Buzz')
+      })
+    });
+  });
+
+  describe('3と5の公倍数を渡した場合', () => {
+    describe('15を渡した場合', () => {
+      it('FizzBuzzを返すこと', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz')
+      })
+    });
+    describe('30を渡した場合', () => {
+      it('FizzBuzzを返すこと', () => {
+        expect(fizzBuzz(30)).toBe('FizzBuzz')
+      })
+    });
+  });
+});
--- daily-angular/src/fizz-buzz-component.ts
+++ daily-angular/src/fizz-buzz-component.ts
@@ -2,23 +2,31 @@ import * as angular from 'angular';
 declare function require(x: string): any;
 
 export function fizzBuzz(num: number): string | number  {
+  if (num % 15 === 0) {
+    return 'FizzBuzz';
+  } else if (num % 3 === 0) {
+    return 'Fizz';
+  } else if (num % 5 === 0) {
+    return 'Buzz';
+  } else {
     return num;
+  }
 }
 
 class FizzBuzzComponent implements angular.IComponentOptions {
-    restrict = 'E';
-    controller = FizzBuzzController;
-    controllerAs = 'ctrl';
-    template = require('./fizz-buzz-component.html')
+  restrict = 'E';
+  controller = FizzBuzzController;
+  controllerAs = 'ctrl';
+  template = require('./fizz-buzz-component.html')
 }
 
 class FizzBuzzController implements angular.IController {
 }
 
 function init () {
-    angular
-        .module('myApp')
-        .directive('fizzBuzz', () => new FizzBuzzComponent());
+  angular
+    .module('myApp')
+    .directive('fizzBuzz', () => new FizzBuzzComponent());
 }
 
 export {init as default};
