d81117e240d11235cb7caaec16ff3269113e5132
Wing In CHAN <wingin.chan@bizreach.co.jp>
2019-09-25 16:53:29 +0900

    WIP size and max

--- daily-scala/app/daily/BTree.scala
+++ daily-scala/app/daily/BTree.scala
@@ -1,6 +1,7 @@
 package daily
 
 trait Node {
+<<<<<<< HEAD
   // ...
 }
 
@@ -10,8 +11,23 @@ case class Branch extends Node {
 
 case class Leaf extends Node {
 // ...
+=======
+  def value: Int
+  def size: Int
+  def max: Int
+}
+
+case class Branch(left: Node, value: Int, right: Node) extends Node {
+  val size = left.size + 1 + right.size
+  val max = 4
+}
+
+case class Leaf(value: Int) extends Node {
+  val size = 1
+  val max = value
+>>>>>>> 7cb0607... WIP size and max
 }
 
 case class BTree(node: Node) {
-  // ...
+  val size = node.size
 }
--- daily-scala/test/daily/BTreeSpec.scala
+++ daily-scala/test/daily/BTreeSpec.scala
@@ -3,5 +3,76 @@ package daily
 import org.scalatest.FunSpec
 
 class BTreeSpec extends FunSpec {
+<<<<<<< HEAD
 
+=======
+  describe("Leaf") {
+    val leaf = Leaf(2)
+    describe("#size") {
+      it("equal 1") {
+        assert(leaf.size == 1)
+      }
+    }
+
+    describe("#max") {
+      it("equal 2") {
+        assert(leaf.max == 2)
+      }
+
+      describe("given Leaf(3)") {
+        val leaf2 = Leaf(3)
+        it("equal 3") {
+          assert(leaf2.max == 3)
+        }
+      }
+    }
+  }
+
+  describe("Branch") {
+    val branch1 = Branch(Leaf(1), 3, Leaf(4))
+    val branch2 = Branch(Branch(Leaf(1), 2, Leaf(3)), 4, Leaf(5))
+    describe("#size") {
+      describe("given Branch(Leaf(1), 3, Leaf(4))") {
+        it("return 3") {
+          assert(branch1.size == 3)
+        }
+      }
+      describe("given Branch(Branch(Leaf(1), 2, Leaf(3)), 4, Leaf(5))") {
+        it("return 5") {
+          assert(branch2.size == 5)
+        }
+      }
+    }
+    describe("#max") {
+      describe("given Branch(Leaf(1), 3, Leaf(4))") {
+        it("return 4") {
+          assert(branch1.max == 4)
+        }
+      }
+      describe("given Branch(Branch(Leaf(1), 2, Leaf(3)), 4, Leaf(5))") {
+        it("return 5") {
+          assert(branch2.max == 5)
+        }
+      }
+    }
+  }
+
+  describe("BTree") {
+    val bTree1 = BTree(Leaf(1))
+    val bTree2 = BTree(Branch(Leaf(1), 2, Leaf(3)))
+    describe("#size") {
+      describe("given BTree(Leaf(1)") {
+        it("return 1") {
+          assert(bTree1.size == 1)
+        }
+      }
+      describe("given Branch(Leaf(1), 2, Leaf(3))") {
+        it("return 3") {
+          assert(bTree2.size == 3)
+        }
+      }
+
+    }
+  }
+>>>>>>> 7cb0607... WIP size and max
 }
