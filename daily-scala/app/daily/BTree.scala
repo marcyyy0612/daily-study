4c018b09cc8e580c070b770c8fde4387867afa69
Wing In CHAN <wingin.chan@bizreach.co.jp>
2019-09-30 11:02:20 +0900

    Implemented Max and Min

--- daily-scala/app/daily/BTree.scala
+++ daily-scala/app/daily/BTree.scala
@@ -4,18 +4,23 @@ trait Node {
   def value: Int
   def size: Int
   def max: Int
+  def min: Int
 }
 
 case class Branch(left: Node, value: Int, right: Node) extends Node {
   val size = left.size + 1 + right.size
-  val max = 4
+  val max = right.max
+  val min = left.min
 }
 
 case class Leaf(value: Int) extends Node {
   val size = 1
   val max = value
+  val min = value
 }
 
 case class BTree(node: Node) {
   val size = node.size
+  val max = node.max
+  val min = node.min
 }
--- daily-scala/test/daily/BTreeSpec.scala
+++ daily-scala/test/daily/BTreeSpec.scala
@@ -23,11 +23,20 @@ class BTreeSpec extends FunSpec {
         }
       }
     }
+
+    describe("#min") {
+      val leaf3 = Leaf(1)
+      it("equal 1") {
+        assert(leaf3.min == 1)
+      }
+    }
   }
 
   describe("Branch") {
     val branch1 = Branch(Leaf(1), 3, Leaf(4))
     val branch2 = Branch(Branch(Leaf(1), 2, Leaf(3)), 4, Leaf(5))
+    val branch3 = Branch(Branch(Leaf(1), 2, Leaf(3)), 4, Branch(Leaf(5),6,Leaf(8)))
+    val branch4 = Branch(Leaf(2), 3, Leaf(4))
     describe("#size") {
       describe("given Branch(Leaf(1), 3, Leaf(4))") {
         it("return 3") {
@@ -51,6 +60,23 @@ class BTreeSpec extends FunSpec {
           assert(branch2.max == 5)
         }
       }
+      describe("given Branch(Branch(Leaf(1), 2, Leaf(3)), 4, Branch(Leaf(5),6,Leaf(8)))") {
+        it("return 8") {
+          assert(branch3.max == 8)
+        }
+      }
+    }
+    describe("#min") {
+      describe("Branch(Leaf(1), 3, Leaf(4))") {
+        it("return 1") {
+          assert(branch1.min == 1)
+        }
+      }
+      describe("Branch(Leaf(2), 3, Leaf(4))") {
+        it("return 2") {
+          assert(branch4.min == 2)
+        }
+      }
     }
   }
 
@@ -68,7 +94,18 @@ class BTreeSpec extends FunSpec {
           assert(bTree2.size == 3)
         }
       }
-
+    }
+    describe("#max") {
+      describe("given BTree(Leaf(1)") {
+        it("return 1") {
+          assert(bTree1.max == 1)
+        }
+      }
+      describe("given BTree(Branch(Leaf(1), 2, Leaf(3)))") {
+        it("return 3") {
+          assert(bTree2.max == 3)
+        }
+      }
     }
   }
 }
